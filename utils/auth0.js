f7e5c7e8958de847208732a4bfb78122b65f87b7
Peter F. Tumulty <ptumulty@sh-law.com>
2020-10-23 22:55:21 -0400

    bug: removed ending forward slash from redirect uri

--- utils/auth0.js
+++ utils/auth0.js
@@ -9,12 +9,14 @@ console.log({
   'process.env.AUTH0_REDIRECT_URI': process.env.AUTH0_REDIRECT_URI
 })
 
+const formattedRedirectUri = process.env.AUTH0_REDIRECT_URI.toString().replace('\/', '');
+
 export default initAuth0({
   domain: process.env.AUTH0_DOMAIN,
   clientId: process.env.AUTH0_CLIENTID,
   clientSecret: process.env.AUTH0_CLIENT_SECRET,
   scope: 'openid profile',
-  redirectUri: process.env.AUTH0_REDIRECT_URI,
+  redirectUri: formattedRedirectUri,
   postLogoutRedirectUri: process.env.AUTH0_POST_LOGOUT_REDIRECT_URI,
   session: {
     // The secret used to encrypt the cookie.
