45ac7d515ba4d523bebe710ed7371d30b38b8ae5
ptums <ptumulty923@gmail.com>
2021-02-18 13:51:34 -0500

    refactored app to use updated version of @types/react and auth0

--- components/CoinImage.tsx
+++ components/CoinImage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { LazyLoadImage } from 'react-lazy-load-image-component'
-import { UserAgent } from '../interfaces'
-import { formatImageUrl } from '../utils/helpers'
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { UserAgent } from '../interfaces';
+import { formatImageUrl } from '../utils/helpers';
 
 type Props = {
   image: string
@@ -9,36 +9,35 @@ type Props = {
   userAgent: UserAgent
 };
 
-
 const CoinImage = ({ image, title, userAgent }: Props): JSX.Element => (
   <>
-   {userAgent.deviceType === 'desktop' && (
-        <LazyLoadImage
-          alt={title}
-          height="auto"
-          src={formatImageUrl(image, null, userAgent.os)}
-          width="100%"
-          effect="blur"
-        />
-      )}
-      {userAgent.deviceType === 'tablet' && (
-        <LazyLoadImage
-          alt={title}
-          height="auto"
-          src={formatImageUrl(image, null, userAgent.os)}
-          width="100%"
-          effect="blur"
-        />
-      )}
-      {userAgent.deviceType === 'mobile' && (
-        <LazyLoadImage
-          alt={title}
-          height="auto"
-          src={formatImageUrl(image, 'c_scale,w_464', userAgent.os)}
-          width="100%"
-          effect="blur"
-        />
-      )}
+    {userAgent.deviceType === 'desktop' && (
+    <LazyLoadImage
+      alt={title}
+      height="auto"
+      src={formatImageUrl(image, null, userAgent.os)}
+      width="100%"
+      effect="blur"
+    />
+    )}
+    {userAgent.deviceType === 'tablet' && (
+    <LazyLoadImage
+      alt={title}
+      height="auto"
+      src={formatImageUrl(image, null, userAgent.os)}
+      width="100%"
+      effect="blur"
+    />
+    )}
+    {userAgent.deviceType === 'mobile' && (
+    <LazyLoadImage
+      alt={title}
+      height="auto"
+      src={formatImageUrl(image, 'c_scale,w_464', userAgent.os)}
+      width="100%"
+      effect="blur"
+    />
+    )}
   </>
 );
 
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,30 +1,18 @@
 import React from 'react';
 import { GetServerSideProps } from 'next';
-import Head from 'next/head';
 import Link from 'next/link';
-import useSWR from 'swr';
 import auth0 from '../../utils/auth0';
-import { getFetcher } from '../../utils/helpers';
+import dbConnect from '../../utils/db-connect';
+import Coins, { ICoins } from '../../models/Coins';
+import CollectionCoinCount, {
+  ICollectionCoinCount,
+} from '../../models/CollectionCoinCount';
 import UploadImage from '../../components/UploadImage';
 import CollectionList from '../../components/CollectionList';
 import AdminCoinList from '../../components/AdminCoinList';
 
-export default function Admin({ authed }): JSX.Element {
-  // get a list of all the collections
-  const { data: listOfCollections, error: collectionsError } = useSWR(
-    '/api/list-all-collections',
-    getFetcher,
-  );
-
-  // get a list of all the coins
-  const { data: listOfCoins, error: coinsError } = useSWR(
-    '/api/list-all-coins',
-    getFetcher,
-  );
-
-  const datainCoinsOrCollections = listOfCollections !== undefined && listOfCoins !== undefined
-    ? listOfCollections.data.length === 0 && listOfCoins.data.length === 0
-    : false;
+export default function Admin({ authed, listOfCoins, listOfCollections }): JSX.Element {
+  const datainCoinsOrCollections = listOfCoins.length > 0 && listOfCollections.length > 0;
 
   return (
     <div>
@@ -39,17 +27,14 @@ export default function Admin({ authed }): JSX.Element {
       )}
       <div className="options">
         <div>
-          {listOfCoins !== undefined && listOfCoins.data.length > 0 && (
-            <AdminCoinList collectionName="none" coinList={listOfCoins.data} />
-          )}
+          {listOfCoins && <AdminCoinList collectionName="none" coinList={listOfCoins} />}
         </div>
         <div>
-          {listOfCollections !== undefined
-            && listOfCollections.data.length > 0 && (
-              <CollectionList
-                authed={authed}
-                collections={listOfCollections.data}
-              />
+          {listOfCollections && (
+          <CollectionList
+            authed={authed}
+            collections={listOfCollections}
+          />
           )}
         </div>
       </div>
@@ -105,7 +90,22 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   const { req, res } = context;
 
   if (typeof window === 'undefined') {
+    await dbConnect();
     const session = await auth0.getSession(req);
+
+    // get all coins
+    const listOfCoins: Array<ICoins> = await Coins.find();
+    const listAllCollections: Array<ICollectionCoinCount> = await CollectionCoinCount.find(
+      {},
+    );
+
+    const formattedCollections = listAllCollections.map((collection) => ({
+      id: collection.categoryId,
+      title: collection.categoryTitle,
+      count: collection.count,
+    }));
+
+    // login constraints
     if (!session || !session.user) {
       res.writeHead(302, {
         Location: '/api/login',
@@ -119,10 +119,14 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
         },
       };
     }
+
+    // login response
     return {
       props: {
         user: session.user,
         authed: true,
+        listOfCoins: JSON.parse(JSON.stringify(listOfCoins)) || [],
+        listOfCollections: JSON.parse(JSON.stringify(formattedCollections)) || [],
       },
     };
   }
